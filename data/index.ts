4440fbf28a98a9f64a61a716b2f9d938db676951
kayode-victor <105973074+kayode-victor@users.noreply.github.com>
2025-09-25 13:07:35 +0100

    data adjust

--- data/index.ts
+++ data/index.ts
@@ -62,8 +62,8 @@ export const gridItems = [
   },
   {
     id: 5,
-    title: "Currently Building a Content Management System",
-    description: "Designed for dynamic university websites and admin control",
+    title: "Currently Building a University Management System",
+    description: "Designed for dynamic university websites, school portal and admin control",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
